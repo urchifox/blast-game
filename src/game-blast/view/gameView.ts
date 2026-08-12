9623e869ff11b7aef1e225552dc4533a050d14cf
urchifox <126313096+urchifox@users.noreply.github.com>
2026-08-12 20:49:43 +0100

    move layout calculations into view

--- src/game-blast/view/gameView.ts
+++ src/game-blast/view/gameView.ts
@@ -17,7 +17,7 @@ import { ModalUI } from "./modalUI"
 import { BOOSTER_NAMES } from "../domain/config"
 import { BoosterUIMap } from "../types"
 import { LayoutUI } from "./layoutUI"
-import { LayoutCalculator } from "../flow/layoutCalculator"
+import { LayoutCalculator } from "./layoutCalculator"
 
 type GameViewProps = Omit<ViewProps, "name">
 
